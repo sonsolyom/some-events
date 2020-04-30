75923bfe91457119102a8f29fd396d0fb134a9eb
sonsolyom <nagydsolyom@users.noreply.github.com>
2020-04-30 15:55:19 +0200

    Creating project
    made html
    little css
    and fun jquery.

---
+++ script.js
@@ -0,0 +1,34 @@
+$('.first-btn').click(() => {
+    if ($('.first-btn').text() !== 'How dare you!?')
+    console.log('Yeah, you clicked me');
+    else
+    console.log('Don\'t be ridicoulos...');
+    
+});
+
+$('.second-btn').click(() => {
+    if ($('.first-btn').text() !== 'How dare you!?'){
+        $('.first-btn').text('How dare you!?');
+    }else {
+        $('.first-btn').text('Click me!');
+    }
+});
+
+$('.third-btn').click(() => {
+    if ($('.third-btn').text() === 'Try me!?'){
+        $('.third-btn').text('THE POWER OF CLICKING!!!');
+        $('.third-btn').css('font-weight', 'bold');
+    }else {
+        $('.third-btn').text('Try me!?');
+        $('.third-btn').css('font-weight', 'normal');
+    }
+});
+
+const colors = ['Red', 'Pink', 'Purple', 'Deep Purple', 'Indigo', 'Blue', 'Light Blue', 'Cyan', 'Aqua', 'Teal', 'Green', 'Light Green', 'Lime', 'Sand', 'Khaki', 'Yellow',
+            'Amber', 'Orange', 'Deep Orange', 'Blue Gray', 'Brown', 'Light Gray', 'Gray', 'Dark Gray', 'Pale Red', 'Pale Yellow', 'Pale Green', 'Pale-Blue'];
+
+
+$('.third-btn').click(() => {
+    let random = Math.floor(Math.random()*colors.length);
+    $('.btn').css('background', colors[random]);
+})
